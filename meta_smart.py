734d1f43265a87912999fb2c64f930e87c53d3e2
Gustavo-sh <santosgustav17@gmail.com>
2026-03-24 08:02:09 -0300

    feat: insert db and integration with matriz system

--- meta_smart.py
+++ meta_smart.py
@@ -2,156 +2,197 @@ import time
 import pyodbc
 import pandas as pd
 import numpy as np 
+from datetime import datetime, timedelta
 
 BASE = """
-    SET NOCOUNT ON;
-    SET XACT_ABORT ON;
+        SET NOCOUNT ON;
+        SET XACT_ABORT ON;
 
-    SELECT atributo, id 
-    INTO #atributos
-    FROM dbo.dim_atributo (NOLOCK)
+        IF OBJECT_ID('tempdb..#atributos') IS NOT NULL DROP TABLE #atributos;
+        IF OBJECT_ID('tempdb..#tipos_indicadores') IS NOT NULL DROP TABLE #tipos_indicadores;
+        IF OBJECT_ID('tempdb..#formatos_indicadores') IS NOT NULL DROP TABLE #formatos_indicadores;
+        IF OBJECT_ID('tempdb..#agg') IS NOT NULL DROP TABLE #agg;
+        IF OBJECT_ID('tempdb..#base') IS NOT NULL DROP TABLE #base;
 
-    SELECT 
-        id_indicador,
-        case when formula_perc_atingimento like '%meta/resultado%' then 'Menor Melhor'
-        when formula_perc_atingimento like '%resultado/meta%' then 'Maior Melhor'
-        when formula_perc_atingimento is null then 'Nenhum'
-        else 'Maior Melhor' end as tipo
-    INTO #tipos_mm
-    FROM rby.indicador (NOLOCK)
-    where formula_perc_atingimento is not null
+        -- Capturar nomes dos atributos
 
-    select id_indicador, case when formato = 'coin' then 'integer' else formato end as formato 
-    into #tipos_ind
-    from rby_indicador (nolock) 
-    where formato is not null
+        SELECT atributo, id 
+        INTO #atributos
+        FROM dbo.dim_atributo (NOLOCK)
 
-    select distinct id_segmento, chave_externa
-    into #hc
-    from rlt.bussola (nolock) 
-    where data BETWEEN DATEADD(D,1,EOMONTH(GETDATE(),-2)) AND EOMONTH(GETDATE(),-1)
+        -- Capturar tipos dos indicadores
 
-    select id_segmento
-    into #segmentos
-    from #hc
-    group by id_segmento
-    having COUNT(chave_externa) > 5
+        SELECT 
+            id_indicador,
+            case when formula_perc_atingimento like '%meta/resultado%' then 'Menor Melhor'
+            when formula_perc_atingimento like '%resultado/meta%' then 'Maior Melhor'
+            when formula_perc_atingimento is null then 'Nenhum'
+            else 'Maior Melhor' end as tipo
+        INTO #tipos_indicadores
+        FROM rby.indicador (NOLOCK)
+        where formula_perc_atingimento is not null
 
-    /* 1️⃣ AGREGAÇÃO (SUM feito apenas uma vez) */
+        -- Capturar formatos dos indicadores
 
-    SELECT
-        id,
-        id_segmento,
-        chave_externa,
-        formula_resultado,
-        formula_meta,
-        formula_atingimento,
+        select id_indicador, case when formato = 'coin' then 'integer' else formato end as formato 
+        into #formatos_indicadores
+        from rby_indicador (nolock) 
+        where formato is not null
 
-        SUM(resultado)  AS s_resultado,
-        SUM(fator)      AS s_fator,
-        SUM(fator_2)    AS s_fator2,
-        SUM(fator_3)    AS s_fator3,
-        SUM(fator_4)    AS s_fator4,
-        SUM(meta)       AS s_meta,
-        SUM(hc)         AS s_hc
+        -- Gerar dados agregados
 
-    INTO #agg
-    FROM rlt.bussola (NOLOCK)
-    WHERE data BETWEEN DATEADD(D,1,EOMONTH(GETDATE(),-2)) AND EOMONTH(GETDATE(),-1)
-    AND id_segmento in (select id_segmento from #segmentos)
-    AND id not in (6,34,15,48,49)
-    and id not in (select id_indicador from rby.indicador (nolock) where formula_meta not like '%fatorCalc%' and formula_meta not like '%meta%' and indicador_nome <> 'Descontinuado')
-    AND id not in (select distinct id_indicador from rby.indicador (nolock) where indicador_nome like '%pausa%')
-    GROUP BY
-        id,
-        id_segmento,
-        chave_externa,
-        formula_resultado,
-        formula_meta,
-        formula_atingimento
+        SELECT
+            id,
+            id_segmento,
+            chave_externa,
+            formula_resultado,
+            formula_meta,
+            formula_atingimento,
+
+            SUM(resultado)  AS s_resultado,
+            SUM(fator)      AS s_fator,
+            SUM(fator_2)    AS s_fator2,
+            SUM(fator_3)    AS s_fator3,
+            SUM(fator_4)    AS s_fator4,
+            SUM(meta)       AS s_meta,
+            SUM(hc)         AS s_hc
+
+        INTO #agg
+        FROM rlt.bussola (NOLOCK)
+        WHERE data BETWEEN DATEADD(D,1,EOMONTH(GETDATE(),-2)) AND EOMONTH(GETDATE(),-1)
+        AND id not in (6,34,15,48,49)
+        and id not in (select id_indicador from rby.indicador (nolock) where formula_meta not like '%fatorCalc%' and formula_meta not like '%meta%' and indicador_nome <> 'Descontinuado')
+        AND id not in (select distinct id_indicador from rby.indicador (nolock) where indicador_nome like '%pausa%')
+        GROUP BY
+            id,
+            id_segmento,
+            chave_externa,
+            formula_resultado,
+            formula_meta,
+            formula_atingimento
 
 
-    /* 2️⃣ APLICAR AS FÓRMULAS */
+        -- Gerar resultado e meta calculado
 
-    SELECT
-        id AS id_indicador,
-        id_segmento,
-        chave_externa,
-        formula_atingimento,
+        SELECT
+            id AS id_indicador,
+            id_segmento,
+            chave_externa,
+            formula_atingimento,
 
-        CASE 
-            WHEN formula_resultado = 2 THEN s_resultado
-            WHEN formula_resultado = 3 THEN iif(s_fator = 0, 0, s_resultado / s_fator)
-            WHEN formula_resultado = 5 THEN (s_resultado / s_fator) - (s_fator2 / s_fator3)
-            WHEN formula_resultado = 6 THEN iif(s_fator2 = 0, 0, s_resultado / s_fator2)
-            WHEN formula_resultado = 7 THEN IIF(s_fator = 0,0,s_fator / s_resultado)
-            WHEN formula_resultado = 8 THEN 1 - (s_resultado / s_fator)
-            WHEN formula_resultado = 9 THEN ((s_resultado / s_fator2) - (s_fator2 / s_fator3)) * 100
-            WHEN formula_resultado = 10 THEN (s_resultado / s_fator) + (s_fator2 / s_fator3)
-            WHEN formula_resultado = 11 THEN IIF((s_fator = 0 OR s_fator4 > s_fator),0,(s_resultado/(s_fator2/s_fator))*(1-(s_fator3/s_fator)))
-            WHEN formula_resultado = 12 THEN (s_resultado - s_fator) / s_fator2
-            WHEN formula_resultado = 13 THEN (s_resultado / s_fator) - (s_fator2 / s_fator)
-        END AS resultado,
+            CASE 
+                WHEN formula_resultado = 2 THEN s_resultado
+                WHEN formula_resultado = 3 THEN iif(s_fator = 0, 0, s_resultado / s_fator)
+                WHEN formula_resultado = 5 THEN (s_resultado / s_fator) - (s_fator2 / s_fator3)
+                WHEN formula_resultado = 6 THEN iif(s_fator2 = 0, 0, s_resultado / s_fator2)
+                WHEN formula_resultado = 7 THEN IIF(s_fator = 0,0,s_fator / s_resultado)
+                WHEN formula_resultado = 8 THEN 1 - (s_resultado / s_fator)
+                WHEN formula_resultado = 9 THEN ((s_resultado / s_fator2) - (s_fator2 / s_fator3)) * 100
+                WHEN formula_resultado = 10 THEN (s_resultado / s_fator) + (s_fator2 / s_fator3)
+                WHEN formula_resultado = 11 THEN IIF((s_fator = 0 OR s_fator4 > s_fator),0,(s_resultado/(s_fator2/s_fator))*(1-(s_fator3/s_fator)))
+                WHEN formula_resultado = 12 THEN (s_resultado - s_fator) / s_fator2
+                WHEN formula_resultado = 13 THEN (s_resultado / s_fator) - (s_fator2 / s_fator)
+            END AS resultado,
 
-        CASE
-            WHEN formula_meta = 1  THEN s_meta
-            WHEN formula_meta = 2  THEN iif(s_fator2 = 0,0,s_fator / s_fator2)
-            WHEN formula_meta = 3  THEN s_fator4
-            WHEN formula_meta = 4  THEN IIF(s_fator3 = 0,0,s_fator4 / s_fator3)
-            WHEN formula_meta = 5  THEN IIF(s_hc = 0,0,s_meta / s_hc)
-            WHEN formula_meta = 6  THEN s_fator2
-            WHEN formula_meta = 7  THEN IIF(s_fator = 0,0,s_meta / s_fator)
-            WHEN formula_meta = 8  THEN IIF(s_fator2 = 0,0,s_meta / s_fator2)
-            WHEN formula_meta = 9  THEN IIF(s_fator4 = 0,0,s_meta / s_fator4)
-            WHEN formula_meta = 10 THEN IIF(s_fator = 0,0,s_fator2 / s_fator)
-            WHEN formula_meta = 11 THEN IIF(s_fator3 = 0,0,s_fator2 / s_fator3)
-        END AS meta
+            CASE
+                WHEN formula_meta = 1  THEN s_meta
+                WHEN formula_meta = 2  THEN iif(s_fator2 = 0,0,s_fator / s_fator2)
+                WHEN formula_meta = 3  THEN s_fator4
+                WHEN formula_meta = 4  THEN IIF(s_fator3 = 0,0,s_fator4 / s_fator3)
+                WHEN formula_meta = 5  THEN IIF(s_hc = 0,0,s_meta / s_hc)
+                WHEN formula_meta = 6  THEN s_fator2
+                WHEN formula_meta = 7  THEN IIF(s_fator = 0,0,s_meta / s_fator)
+                WHEN formula_meta = 8  THEN IIF(s_fator2 = 0,0,s_meta / s_fator2)
+                WHEN formula_meta = 9  THEN IIF(s_fator4 = 0,0,s_meta / s_fator4)
+                WHEN formula_meta = 10 THEN IIF(s_fator = 0,0,s_fator2 / s_fator)
+                WHEN formula_meta = 11 THEN IIF(s_fator3 = 0,0,s_fator2 / s_fator3)
+            END AS meta
 
-    INTO #base
-    FROM #agg
+        INTO #base
+        FROM #agg
 
-    SELECT
-        a.atributo,
-        b.id_indicador,
-        chave_externa AS operador,
-        resultado,
-        meta,
-        formula_atingimento,
-        g1,
-        g2,
-        g3,
-        t.tipo,
-        ti.formato,
-        faixa_inferior,
-        faixa_superior
-    FROM #base b
-    JOIN #atributos a ON b.id_segmento = a.id
-    LEFT JOIN dbo.faixa_grupos f 
-        ON f.Id_Indicador = b.id_indicador
-        AND f.data = EOMONTH(GETDATE(), -1)
-    LEFT JOIN #tipos_mm t
-        ON b.id_indicador = t.id_indicador
-    left join #tipos_ind ti
-        on b.id_indicador = ti.id_indicador
-    left join faixas_meta_smart fms
-        on b.id_indicador = fms.id
+        -- Query final
 
-    drop table #atributos
-    drop table #tipos_mm
-    drop table #tipos_ind
-    drop table #agg
-    drop table #base
-    drop table #hc
-    drop table #segmentos
+        SELECT
+            a.atributo,
+            b.id_indicador,
+            chave_externa AS operador,
+            resultado,
+            meta,
+            formula_atingimento,
+            g1,
+            g2,
+            g3,
+            t.tipo,
+            ti.formato,
+            faixa_inferior,
+            faixa_superior
+        FROM #base b
+        JOIN #atributos a ON b.id_segmento = a.id
+        LEFT JOIN dbo.faixa_grupos f 
+            ON f.Id_Indicador = b.id_indicador
+            AND f.data = EOMONTH(GETDATE(), -1)
+        LEFT JOIN #tipos_indicadores t
+            ON b.id_indicador = t.id_indicador
+        left join #formatos_indicadores ti
+            on b.id_indicador = ti.id_indicador
+        left join faixas_meta_smart (nolock) fms
+            on b.id_indicador = fms.id
+
+        drop table #atributos
+        drop table #tipos_indicadores
+        drop table #formatos_indicadores
+        drop table #agg
+        drop table #base
     """
 
-CONNECTION_STRING = "Driver={SQL Server};Server=primno4;Database=Robbyson;Trusted_Connection=yes;"
+CONNECTION_STRING = "Driver={SQL Server};Server=primno4;Database=Robbyson;Trusted_Connection=yes;Timeout=60;"
 
 def get_base():
     conn = pyodbc.connect(CONNECTION_STRING)
     df = pd.read_sql(BASE, conn)
     conn.close()
     return df
+
+def insert_result(df):
+
+    conn = pyodbc.connect(CONNECTION_STRING)
+    cursor = conn.cursor()
+
+    insert_sql = """
+    INSERT INTO dbo.meta_smart (
+        data_referencia,
+        data_insert,
+        atributo,
+        id_indicador,
+        meta_smart,
+        meta_original,
+        p_g1g2,
+        tipo_meta
+    )
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    """
+
+    dados = [
+        (
+            row.data_referencia,
+            row.data_insert,
+            row.atributo,
+            row.id_indicador,
+            row.meta_otimizada,
+            row.meta_original,
+            row.p_g1g2,
+            row.tipo_meta
+        )
+        for row in df.itertuples(index=False)
+    ]
+
+    cursor.fast_executemany = True
+    cursor.executemany(insert_sql, dados)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 def calc_p_g1g2(df, meta):
 
@@ -352,18 +393,29 @@ def main():
         if formato == "integer":
             meta_otima = round(meta_otima)
             p = calc_p_g1g2(grupo, meta_otima)
+        elif formato == "hour":
+            meta_otima = meta_otima/86400
+            meta_original = meta_original/86400
 
         resultados.append({
             "atributo": atributo,
             "id_indicador": indicador,
-            "meta_otimizada": round(meta_otima, 3),
-            "meta_original": round(meta_original, 3),
-            "p_g1g2": round(p, 3),
+            "meta_otimizada": meta_otima,
+            "meta_original": meta_original,
+            "p_g1g2": p,
             "tipo_meta": tipo_meta
         })
 
     print("for executado em ", (time.time() - start_for))
     resultado_final = pd.DataFrame(resultados)
+
+    hoje = datetime.now()
+    primeiro_dia_mes_atual = hoje.replace(day=1)
+    data_referencia = (primeiro_dia_mes_atual - timedelta(days=1)).replace(day=1).strftime("%Y-%m-%d")
+    data_insert = hoje.strftime("%Y-%m-%d")
+
+    resultado_final.insert(0, "data_insert", data_insert)
+    resultado_final.insert(0, "data_referencia", data_referencia)
 
     arquivo_saida = "meta_smart.xlsx"
 
@@ -372,6 +424,7 @@ def main():
         index=False
     )
 
+    insert_result(resultado_final)
 
 if __name__ == "__main__":
     main()
